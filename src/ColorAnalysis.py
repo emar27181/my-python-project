67622c0efe8495baef4bdd1c985b8606a048614c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-08 01:00:38 +0900

    入力された文字とその品詞が表示されるように変更

--- src/ColorAnalysis.py
+++ src/ColorAnalysis.py
@@ -3,11 +3,14 @@ with open("data/input/ColorInput.txt", "r") as file:
     inputText = file.read()
 print("inputText:\n " + inputText + "\n")
 
-# mecab = MeCab.Tagger("-Owakati")
-mecab = MeCab.Tagger()
-result = mecab.parse(inputText)
+mecab = MeCab.Tagger("-Owakati")
+node = mecab.parseToNode(inputText)
 
-print("result all: \n" + result)
+while node:
+    print(node.surface+': '+node.feature.split(",")[0])
+    node = node.next
 
+"""
 with open("data/output/ColorOutput.txt", "w") as file:
     file.write(result)
+"""
