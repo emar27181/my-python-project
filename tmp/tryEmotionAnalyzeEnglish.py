d6c8bc44a538dc1dfb650d1a296948f938a0b6f4
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-19 19:09:33 +0900

    様々な文章での分類テストの追記

--- tmp/tryEmotionAnalyzeEnglish.py
+++ tmp/tryEmotionAnalyzeEnglish.py
@@ -2,10 +2,12 @@
 from transformers import pipeline
 classifier = pipeline("sentiment-analysis")
 
-# 「I hate you（嫌い）」を感情分析
-result = classifier("I hate you")[0]
-print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
+paragraphs = ["I hate you", "I love you", "I like you", "I don't hate you", "I don't love you", "I don't like you",
+              "I have a big dream", "It is difficult for me to speak English well",
+              "I have a neutral opinion", "deep blue", "shiny red", "deep black", "deep white",
+              "dackd desac", "hoge hoge", "positive negative", "negative positive"]
 
-# 「I love you（好き）」を感情分析
-result = classifier("I love you")[0]
-print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
+for paragraph in paragraphs:
+    result = classifier(paragraph)[0]
+    print(
+        f"label: {result['label']}, with score: {round(result['score'], 4)}, paragraph: {paragraph}")
