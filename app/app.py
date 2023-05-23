2fbf93703f873a3a32cd87e8b27ad0518c003367
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-23 19:21:21 +0900

    データの送信または受信によって異なるURLを使用するように仕様を変更

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, redirect, url_for
 from flask_cors import CORS
+import logging
 
 app = Flask(__name__)
 CORS(app)
@@ -7,18 +8,24 @@ CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    app.logger.debug("This is a debug test message(logging)")
+    print("This is a debug test message(print)")
     return redirect('/api/data')
 
 
-@app.route('/api/data', methods=['GET', 'POST'])
-def post_data():
+@app.route('/api/send-data', methods=['GET', 'POST'])
+def send_data():  # データの送信
+    print("post_data() is called(送信)")
     data = {'message': 'This is the data from Flask!'}
     return jsonify(data)
 
 
-def fetch_data():
-    data = request.json.get('data')  # 2 フロントエンドから受信したデータ
-    {"greeting": "Hello! {}".format(data)}
+@app.route('/api/receive-data', methods=['GET', 'POST'])
+def receive_data():  # データの受信
+    print("fetch_data() is called(受信)")
+    data = request.json.get('data')  # フロントエンドからデータを受信
+    print("This is a debug test message(print)")
+    print("data: ", data)
 
 
 if __name__ == '__main__':
