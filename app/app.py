87a685f78e54ae42472a17ebf4c077db156bc075
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-23 17:21:09 +0900

    app.pyでデータを更新できるように変更

--- app/app.py
+++ app/app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, redirect, url_for
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    data = request.json.get('data')  # 2 フロントエンドから受信したデータ
+    {"greeting": "Hello! {}".format(data)}
+
+
+@app.route('/api/data', methods=['GET', 'POST'])
+def get_data():
+    data = {'message': 'This is the data from Flask!'}
+
+    """
+    data = [
+        {"id": 1, "name": "データ1"},
+        {"id": 2, "name": "データ2"},
+        {"id": 3, "name": "データ3"},
+    ]
+    """
+    return jsonify(data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=5000, debug=True)
