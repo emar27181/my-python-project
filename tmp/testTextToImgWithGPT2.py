1ede3c9f393fd7c86b164db79bd160fb0ced3657
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-09-23 21:32:05 +0900

    マスクが塗られた箇所のみを編集する機能の仮実装

--- tmp/testTextToImgWithGPT2.py
+++ tmp/testTextToImgWithGPT2.py
@@ -9,6 +9,7 @@ NUMBER_OF_IMAGES = 2
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+
 """
 response = openai.Image.create(
     # image=open("data/output/image_test.png", "rb"),
@@ -19,6 +20,8 @@ response = openai.Image.create(
 )
 """
 
+
+"""
 response = openai.Image.create_variation(
     image=open("data/output/image_0.png", "rb"),
     # prompt="An impressionist painter's illustration of three calico cats playing together.",
@@ -26,6 +29,21 @@ response = openai.Image.create_variation(
     size="512x512",
     response_format="b64_json",
 )
+"""
+
+
+response = openai.Image.create_edit(
+    image=open("data/input/test_input_by_p5.png", "rb"),
+    #mask=open("data/input/test_input_by_p5.png", "rb"),
+    #mask=open("data/input/input_white.png", "rb"),
+    mask=open("data/input/input_black.png", "rb"),
+    prompt="An impressionist painter's illustration of three calico cats playing together.",
+    n=NUMBER_OF_IMAGES,
+    size="512x512",
+    response_format="b64_json",
+)
+
+
 
 for data, n in zip(response["data"], range(NUMBER_OF_IMAGES)):
     img_data = base64.b64decode(data["b64_json"])
