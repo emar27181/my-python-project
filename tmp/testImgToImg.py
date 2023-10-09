5d4809d28e825cd280796c003884e84cc5fa0ba9
ema_r <ee207008@meiji.ac.jp>
2023-10-09 11:52:20 +0000

    ファイルを読み込んでimg2imgを行う機能の実装

--- tmp/testImgToImg.py
+++ tmp/testImgToImg.py
@@ -4,28 +4,28 @@ from PIL import Image
 from io import BytesIO
 from diffusers import StableDiffusionImg2ImgPipeline
 
-# device = "cuda"
-device = "cpu"
+device = "cuda"
 pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
     "nitrosocke/Ghibli-Diffusion",
 ).to(device)
 
-url = "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/assets/stable-samples/img2img/sketch-mountains-input.jpg"
-
-response = requests.get(url)
-init_image = Image.open(BytesIO(response.content)).convert("RGB")
+# init_image = Image.open("data/output/saveCanvas - 2023-10-05T212050.177.png").convert("RGB")
+init_image = Image.open("data/output/img_to_img_after_3.jpg").convert("RGB")
 init_image.thumbnail((768, 768))
 init_image
 
 print(init_image)
 
-with open('data/output/img_to_img.jpg', 'wb') as image:
-    init_image.save(image, format='JPEG')
+with open('data/output/img_to_img.jpg', 'wb') as input_file:
+    init_image.save(input_file, format='JPEG')
 
-prompt = "ghibli style, a fantasy landscape with castles"
+prompt = "meteor, oil painting, fantastic"
+# prompt = "oil painting, Hellfire, fantastic, sacred water"
+# prompt = "ukiyoe style, yellow mountain, beautiful lake and sea, orange villages"
+# prompt = "ghibli style, a fantasy landscape with castles"
 generator = torch.Generator(device=device).manual_seed(1024)
 image = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5, generator=generator).images[0]
 image
 
-with open('data/output/img_to_img_after.jpg', 'wb') as file:
-    file.save(image, format='JPEG')
+with open('data/output/img_to_img_after.jpg', 'wb') as input_file:
+    image.save(input_file, format='JPEG')
