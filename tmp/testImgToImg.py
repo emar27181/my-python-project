47e0baa5e98077f651e29b5505b52b8a7f3da8f2
ema_r <ee207008@meiji.ac.jp>
2023-10-11 17:52:01 +0900

    不要なコメントアウトの削除

--- tmp/testImgToImg.py
+++ tmp/testImgToImg.py
@@ -13,7 +13,9 @@ device = "cuda"
 pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
     "nitrosocke/Ghibli-Diffusion",
 ).to(device)
-init_image = Image.open("data/output/before/saveCanvas - 2023-10-05T212050.177.png").convert("RGB")
+# init_image = Image.open("data/output/before/saveCanvas - 2023-10-10T194006.802.png").convert("RGB")
+init_image = Image.open("data/output/after/img_to_img_after_2023-10-10_19-51-34.jpg").convert("RGB")
+# init_image = Image.open("data/output/before/saveCanvas - 2023-10-05T212050.177.png").convert("RGB")
 # init_image = Image.open("data/output/saveCanvas - 2023-10-10T124818.646.png").convert("RGB")
 # init_image = Image.open("data/output/img_to_img_after_3.jpg").convert("RGB")
 init_image.thumbnail((768, 768))
@@ -25,14 +27,10 @@ file_name_before = 'data/output/before/img_to_img_before_{}.jpg'.format(current_
 file_name_after = 'data/output/after/img_to_img_after_{}.jpg'.format(current_time_str)
 random_number = random.randint(1, 1024)
 
-#with open('data/output/img_to_img.jpg', 'wb') as input_file:
 with open(file_name_before, 'wb') as input_file:
     init_image.save(input_file, format='JPEG')
 
-prompt = "meteor, oil painting, fantastic"
-# prompt = "oil painting, Hellfire, fantastic, sacred water"
-# prompt = "ukiyoe style, yellow mountain, beautiful lake and sea, orange villages"
-# prompt = "ghibli style, a fantasy landscape with castles"
+prompt = "tiger eye, oil painting, fantastic, black eye"
 generator = torch.Generator(device=device).manual_seed(random_number)
 image = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5, generator=generator).images[0]
 image
