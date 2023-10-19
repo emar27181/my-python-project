cd7380f6ed1661c8c38218d8bb60427afbba7c03
ema_r <ee207008@meiji.ac.jp>
2023-10-19 12:42:45 +0900

    記録データに"strength_value"と"guidance_scale_value"を追加

--- tmp/testImgToImg.py
+++ tmp/testImgToImg.py
@@ -12,7 +12,7 @@ pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
     "nitrosocke/Ghibli-Diffusion",
 ).to(device)
 
-input_file_path = "data/output/after/img_to_img_after_2023-10-10_19-51-34.jpg"
+input_file_path = "data/output/before/saveCanvas - 2023-10-15T185638.294.png"
 init_image = Image.open(input_file_path).convert("RGB")
 init_image.thumbnail((768, 768))
 init_image
@@ -21,13 +21,13 @@ current_time = datetime.now()
 current_time_str=current_time.strftime('%Y-%m-%d_%H-%M-%S')
 output_file_path = 'data/output/after/img_to_img_after_{}.jpg'.format(current_time_str)
 random_number = random.randint(1, 1024)
-
-
-
 with open('data/input/input_prompt.txt', "r") as input_file:
     prompt = input_file.read()
+strength_value=0.75
+guidance_scale_value=7.5
+
 generator = torch.Generator(device=device).manual_seed(random_number)
-image = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5, generator=generator).images[0]
+image = pipe(prompt=prompt, image=init_image, strength=strength_value, guidance_scale=guidance_scale_value, generator=generator).images[0]
 image
 
 with open(output_file_path, 'wb') as input_file:
@@ -38,6 +38,8 @@ new_data = {
     "prompt": prompt,
     "create_time": current_time_str,
     "seed": random_number,
+    "strength": strength_value,
+    "guidance_scale": guidance_scale_value,
     "input_file": input_file_path, 
     "output_file": output_file_path
 }
