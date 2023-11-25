d791586f5f2f6fb1506c0a5df1c6faaa31560d10
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-25 21:33:48 +0900

    データを更新する機能の初期実装

--- src/color_recommendation/color_recommendation.py
+++ src/color_recommendation/color_recommendation.py
@@ -1,6 +1,11 @@
+import json
+json_file_path = 'data/output/output_color_combination.json'
+
+
 def main():
-    #my_color = Color(360, 50, 50)
-    my_color = Color(480, 150, 150)
+    # my_color = Color(360, 50, 50)
+    # my_color = Color(480, 150, 150)
+    my_color = Color(480, 0, 0)
     generate_color_combination(my_color)
 
 
@@ -13,25 +18,43 @@ class Color:
     def __str__(self):
         return f"Color(hue={self.hue}, saturation={self.saturation}, lightness={self.lightness})"
 
-# 色の組み合わせを返す関数
+
+def updateJson(new_color):
+    new_data = {
+        "hue": new_color.hue,
+        "saturation": new_color.saturation,
+        "lightness": new_color.lightness
+    }
+
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+        data.append(new_data)
+
+    with open(json_file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
+
+
 def generate_color_combination(color):
+    # 色の組み合わせを返す関数
     print("color(" + str(color.hue) + ", " +
           str(color.saturation) + ", " + str(color.lightness) + ")")
 
     # 同一色相配色
-    same_hue_color_scheme(color)
+    new_color = same_hue_color_scheme(color)
 
     # 隣接色相配色
     # 類似色相配色
     # 中差色相配色
     # 対照色相配色
     # 補色色相配色
+    updateJson(new_color)
 
 
 def same_hue_color_scheme(color):
     new_color = Color(color.hue, color.saturation - 30, color.lightness - 30)
     print("new_color(" + str(new_color.hue) + ", " +
           str(new_color.saturation) + ", " + str(new_color.lightness) + ")")
+    return (new_color)
 
 
 if __name__ == "__main__":
