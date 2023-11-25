4a364eeb088066216959b26af9712b0df4ff6c4d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-25 19:28:37 +0900

    色のクラスの初期実装

--- src/color_recommendation/color_recommendation.py
+++ src/color_recommendation/color_recommendation.py
@@ -1,9 +1,22 @@
-def generate_color_combination(color):
-    print("color is " + color)
 
 
 def main():
-    generate_color_combination("red")
+    # generate_color_combination("red")
+    my_color = Color(255, 255, 255)
+    print (my_color)
+
+
+class Color:
+    def __init__(self, hue, saturation, lightness):
+        self.hue = hue
+        self.saturation = saturation
+        self.lightness = lightness
+
+    def __str__(self):
+        return f"Color(hue={self.hue}, saturation={self.saturation}, lightness={self.lightness})"
+
+def generate_color_combination(color):
+    print("color is " + color)
 
 
 if __name__ == "__main__":
