63e18f0ee9fc73c812e55cca34f10d9a0ccb9b9b
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-25 20:43:15 +0900

    同一色相配色の例を返す関数の初期実装

--- src/color_recommendation/color_recommendation.py
+++ src/color_recommendation/color_recommendation.py
@@ -1,8 +1,7 @@
-
-
 def main():
-    my_color = Color(255, 255, 255)
+    my_color = Color(360, 50, 50)
     generate_color_combination(my_color)
+
 
 class Color:
     def __init__(self, hue, saturation, lightness):
@@ -13,8 +12,25 @@ class Color:
     def __str__(self):
         return f"Color(hue={self.hue}, saturation={self.saturation}, lightness={self.lightness})"
 
+# 色の組み合わせを返す関数
 def generate_color_combination(color):
-    print(color.hue, color.saturation, color.lightness)
+    print("color(" + str(color.hue) + ", " +
+          str(color.saturation) + ", " + str(color.lightness) + ")")
+
+    # 同一色相配色
+    same_hue_color_scheme(color)
+
+    # 隣接色相配色
+    # 類似色相配色
+    # 中差色相配色
+    # 対照色相配色
+    # 補色色相配色
+
+
+def same_hue_color_scheme(color):
+    new_color = Color(color.hue, color.saturation - 30, color.lightness - 30)
+    print("new_color(" + str(new_color.hue) + ", " +
+          str(new_color.saturation) + ", " + str(new_color.lightness) + ")")
 
 
 if __name__ == "__main__":
