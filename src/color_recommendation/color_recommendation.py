298ed14d1ab5c29e415141f768c4699c77aa6a31
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-25 20:45:55 +0900

    HSL値がそれぞれ適切な範囲以外に収まるように修正

--- src/color_recommendation/color_recommendation.py
+++ src/color_recommendation/color_recommendation.py
@@ -1,13 +1,14 @@
 def main():
-    my_color = Color(360, 50, 50)
+    #my_color = Color(360, 50, 50)
+    my_color = Color(480, 150, 150)
     generate_color_combination(my_color)
 
 
 class Color:
     def __init__(self, hue, saturation, lightness):
-        self.hue = hue
-        self.saturation = saturation
-        self.lightness = lightness
+        self.hue = hue % 360
+        self.saturation = saturation % 100
+        self.lightness = lightness % 100
 
     def __str__(self):
         return f"Color(hue={self.hue}, saturation={self.saturation}, lightness={self.lightness})"
