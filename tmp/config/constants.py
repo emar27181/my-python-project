226f9c520a1c192a88e2d728ecc2b81f979de7dd
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-25 01:08:55 +0900

    どのタイミングに塗られた色を評価するか保存する配列の実装

--- tmp/config/constants.py
+++ tmp/config/constants.py
@@ -1,3 +1,4 @@
 # 固定値の宣言
 SIM_VALUE_IS_SAME_COLOR = 10  # 同じ色かどうか判定する閾値(10が良さそう？)
 EVALUATED_ILLUST_COUNT = 10  # 次の色が含まれているかどうか比較されたイラストの枚数
+IS_EVALUATED_TIMING_DRAW_COLOR = [0]  # どのタイミングに塗られた色を評価するか保存する配列
