bb11901c3def90400788b5c599e1da255656f32a
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-24 23:17:31 +0900

    recall@kのグラフ生成と同時にprecision@kのグラフが生成されるように変更

--- tmp/recall@k.py
+++ tmp/recall@k.py
@@ -14,6 +14,7 @@ recalls = data
 
 # 配列の宣言
 recall_at_k_values = []
+precision_at_k_values = []
 k_values = []
 
 # 配列に値の代入
@@ -21,7 +22,12 @@ for item in recalls:
     recall = item.get('recall')
     k = item.get('k')
 
+    if (k == 0):
+        continue
+
+    corecct_number = recall * (config.constants.EVALUATED_ILLUST_COUNT)
     recall_at_k_values.append(recall)
+    precision_at_k_values.append(corecct_number / k)
     k_values.append(k)
 
 
@@ -32,19 +38,32 @@ def recall_at_k(true_labels, pred_scores, k):
     return relevant_at_k / k
 
 
-print(recall_at_k_values)
-
 # k_values = range(1, len(pred_scores) + 1)
 # p_at_k_values = [recall_at_k(true_labels, pred_scores, k) for k in k_values]
+# print(precision_at_k_values)
+# print(recall_at_k_values)
 
 plt.figure(figsize=(10, 6))
 plt.plot(k_values, recall_at_k_values, marker='o')
 plt.title(f'recall@k (SAME={config.constants.SIM_VALUE_IS_SAME_COLOR})')
 plt.xlabel('K')
-plt.ylabel('recall')
+plt.ylabel('Recall')
 plt.xticks(k_values)
 plt.grid(True)
 
 # グラフをファイルに保存
+plt.savefig(f'/mnt/c/WSL-directory/my-NLP-project/tmp/output/{file_name}.png')
+print(f"{file_name}.png is saved")
+
+plt.figure(figsize=(10, 6))
+plt.plot(k_values, precision_at_k_values, marker='o')
+plt.title(f'Precision@k (SAME={config.constants.SIM_VALUE_IS_SAME_COLOR})')
+plt.xlabel('K')
+plt.ylabel('Precision')
+plt.xticks(k_values)
+plt.grid(True)
+
+# グラフをファイルに保存
+file_name = (f'precision@k(SAME={config.constants.SIM_VALUE_IS_SAME_COLOR})')
 plt.savefig(f'/mnt/c/WSL-directory/my-NLP-project/tmp/output/{file_name}.png')
 print(f"{file_name}.png is saved")
