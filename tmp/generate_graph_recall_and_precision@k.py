4645e89e19a77c38f52ed1d798b37f8c6c13161d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-27 00:19:54 +0900

    main文内のSAMEとTIMEに読み込むjsonファイルが対応するように変更

--- tmp/generate_graph_recall_and_precision@k.py
+++ tmp/generate_graph_recall_and_precision@k.py
@@ -25,10 +25,9 @@ def insert_values(recalls):
 
 
 # 引数で受け取った値のグラフを作成する関数
-def generate_graph(graph_name, y_values):
-    plt.figure(figsize=(10, 6))
+def generate_graph(graph_name, y_values, SAME, TIME):
     plt.plot(k_values, y_values, marker='o')
-    plt.title(f'{graph_name}@k (SAME={config.constants.SIM_VALUE_IS_SAME_COLOR}, EVAL={config.constants.IS_EVALUATED_TIMING_DRAW_COLOR},n={config.constants.EVALUATED_ILLUST_COUNT})')
+    plt.title(f'{graph_name}@k (SAME={SAME}, EVAL={TIME},n={config.constants.EVALUATED_ILLUST_COUNT})')
     plt.ylim(0, 1)
     plt.xlabel('K')
     plt.ylabel(f'{graph_name}')
@@ -36,8 +35,23 @@ def generate_graph(graph_name, y_values):
     plt.grid(True)
 
 
+def return_data(SAME, TIME):
+    file_name = (f'recall@k_SAME={SAME}_EVAL={TIME}')
+    file_path = (f'tmp/input/{file_name}.json')
+
+    # jsonデータの読み込み
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
 def main():
+
+    SAME = 15
+    TIME = [0, 1]
+
     # ファイル名を変数を使って動的に生成する
+    """
     file_name = (f'recall@k_SAME={config.constants.SIM_VALUE_IS_SAME_COLOR}_EVAL={config.constants.IS_EVALUATED_TIMING_DRAW_COLOR}')
     file_path = (f'tmp/input/{file_name}.json')
 
@@ -45,21 +59,29 @@ def main():
     with open(file_path, 'r') as file:
         data = json.load(file)
     recalls = data
+    """
+
+    plt.figure(figsize=(10, 6))
+    recalls = return_data(SAME, TIME)
 
     insert_values(recalls)
 
     # recall@kグラフの生成
-    generate_graph('recall', recall_at_k_values)
+    generate_graph('recall', recall_at_k_values, SAME, TIME)
 
     # グラフの保存
+    file_name = (f'recall@k_SAME={SAME}_EVAL={TIME}')
     plt.savefig(f'/mnt/c/WSL-directory/my-NLP-project/tmp/output/{file_name}.png')
     print(f"./tmp/output/{file_name}.png is saved")
 
+    plt.figure(figsize=(10, 6))
+
     # precision@kグラフの作成
-    generate_graph('precision', precision_at_k_values)
+    generate_graph('precision', precision_at_k_values, SAME, TIME)
 
     # グラフをファイルに保存
-    precision_file_name = (f'precision@k_SAME={config.constants.SIM_VALUE_IS_SAME_COLOR}_EVAL={config.constants.IS_EVALUATED_TIMING_DRAW_COLOR}')
+    precision_file_name = (f'precision@k_SAME={SAME}_EVAL={TIME}')
+    # precision_file_name = (f'precision@k_SAME={config.constants.SIM_VALUE_IS_SAME_COLOR}_EVAL={config.constants.IS_EVALUATED_TIMING_DRAW_COLOR}')
     plt.savefig(f'/mnt/c/WSL-directory/my-NLP-project/tmp/output/{precision_file_name}.png')
     print(f"./tmp/output/{precision_file_name}.png is saved")
 
