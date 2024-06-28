684df7e0358188253182f3faa85f6e790da5d44e
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-28 18:35:13 +0900

    同一色判定の閾値を10に固定

--- tmp/config/constants.py
+++ tmp/config/constants.py
@@ -3,5 +3,5 @@ SIM_VALUE_IS_SAME_COLOR = 10  # 同じ色かどうか判定する閾値(10が良
 EVALUATED_ILLUST_COUNT = 10  # 次の色が含まれているかどうか比較されたイラストの枚数
 IS_EVALUATED_TIMING_DRAW_COLOR = [0, 1]  # どのタイミングに塗られた色を評価するか保存する配列
 
-SIM_MIN = 5
-SIM_MAX = 20
+SIM_MIN = 10
+SIM_MAX = 10
