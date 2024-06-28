616f90d0d8856c23a4fd651d055e675f85e8c1dd
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-28 16:10:42 +0900

    不要なコメントアウトの削除

--- tmp/generate_graph_recall_and_precision@k.py
+++ tmp/generate_graph_recall_and_precision@k.py
@@ -91,44 +91,10 @@ def load_file_and_generate_graph(graph_type):
 
 def main():
 
+    # recall@kのグラフの作成
     load_file_and_generate_graph('recall')
+    # precision@kのグラフの作成
     load_file_and_generate_graph('precision')
-
-    # recall@kグラフの生成
-    """
-    plt.figure(figsize=(10, 6))  # グラフの初期化
-    for idx, SAME in enumerate(range(config.constants.SIM_MIN, config.constants.SIM_MAX + 1, 5)):
-        # ファイルデータの取得
-        recalls = return_data(SAME, TIME)
-
-        # 配列のデータの更新
-        recall_at_k_values, precision_at_k_values, k_values = insert_values(recalls)
-        color = colors[idx % len(colors)]
-        label = f'SAME={SAME}'
-        generate_graph('recall', k_values, recall_at_k_values, SAME, TIME, color, label)
-
-    # グラフの保存
-    file_name = f'recall@k_SAME={config.constants.SIM_MIN}~{config.constants.SIM_MAX}_EVAL={TIME}'
-    plt.savefig(f'/mnt/c/WSL-directory/my-NLP-project/tmp/output/{file_name}.png')
-    print(f"./tmp/output/{file_name}.png is saved")
-
-    # precision@kグラフの作成
-    plt.figure(figsize=(10, 6))
-    for idx, SAME in enumerate(range(config.constants.SIM_MIN, config.constants.SIM_MAX + 1, 5)):
-        # ファイルデータの取得
-        recalls = return_data(SAME, TIME)
-
-        # 配列のデータの更新
-        recall_at_k_values, precision_at_k_values, k_values = insert_values(recalls)
-        color = colors[idx % len(colors)]
-        label = f'SAME={SAME}'
-        generate_graph('precision', k_values, precision_at_k_values, SAME, TIME, color, label)
-
-    # グラフをファイルに保存
-    file_name = f'precision@k_SAME={config.constants.SIM_MIN}~{config.constants.SIM_MAX}_EVAL={TIME}'
-    plt.savefig(f'/mnt/c/WSL-directory/my-NLP-project/tmp/output/{file_name}.png')
-    print(f"./tmp/output/{file_name}.png is saved")
-    """
 
 
 if __name__ == "__main__":
