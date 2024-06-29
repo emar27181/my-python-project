57ed4c68b719424b173a68d6e2e4b969b77e4bce
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-29 18:05:00 +0900

    評価されるパラメータを保存する文字列の内容によって生成されるグラフのパラメータが対応するように変更

--- tmp/config/constants.py
+++ tmp/config/constants.py
@@ -3,9 +3,30 @@ EVALUATED_ILLUST_COUNT = 10  # 次の色が含まれているかどうか比較�
 IS_EVALUATED_TIMING_DRAW_COLOR = [0, 1]  # どのタイミングに塗られた色を評価するか保存する配列
 
 # 現在使用している固定値は以下
-LIGHTNESS_LIST = [[], [-10, 10], [-20, 20]]  # どの明度の差によってバリエーションが追加されたかを保存する配列
-TIME_LIST = [[1]]
-SIM_MIN = 10
-SIM_MAX = 10
+LIGHTNESS_LIST = []  # どの明度の差によってバリエーションが追加されたかを保存する配列
+TIME_LIST = []
+SIM_MIN = 0
+SIM_MAX = 0
 
-EVALUATED_PARAMETER = "LIGHT"  # "SAME", "TIME", "LIGHT"
+EVALUATED_PARAMETER = "SAME"  # "SAME", "TIME", "LIGHT"
+
+# 同一色判定の閾値の差が異なる固定値の設定
+if (EVALUATED_PARAMETER == "SAME"):
+    LIGHTNESS_LIST = [[-20, 20]]
+    TIME_LIST = [[0, 1]]
+    SIM_MIN = 5
+    SIM_MAX = 15
+
+# 明度のバリエーションが異なる固定値の設定
+elif (EVALUATED_PARAMETER == "LIGHT"):
+    LIGHTNESS_LIST = [[], [-10, 10], [-20, 20]]
+    TIME_LIST = [[1]]
+    SIM_MIN = 10
+    SIM_MAX = 10
+
+# 描画するタイミングが異なる固定値の設定
+elif (EVALUATED_PARAMETER == "TIME"):
+    LIGHTNESS_LIST = [[-20, 20]]
+    TIME_LIST = [[0], [1], [0, 1]]
+    SIM_MIN = 10
+    SIM_MAX = 10
