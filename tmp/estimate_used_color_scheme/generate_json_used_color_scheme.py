db89f1b3ae6749ff43d829b1aaf141286a0e3d09
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-15 19:39:41 +0900

    配色を保存する先頭の要素にイラストの名前(ファイルのパス)が追加されるように変更

--- tmp/estimate_used_color_scheme/generate_json_used_color_scheme.py
+++ tmp/estimate_used_color_scheme/generate_json_used_color_scheme.py
@@ -18,6 +18,14 @@ def generate_json_used_color_scheme(image_path):
         }
         json_data.append(color_dict)
 
+    first_element = {
+        "illustName": image_path,
+        "color": json_data[0]["color"],
+        "rate": json_data[0]["rate"]
+    }
+
+    json_data = [first_element] + json_data[1:]
+
     json_string = json.dumps(json_data, indent=4)  # JSON文字列に変換
     # print(json_string)  # 結果のJSON文字列を表示
     # print(json_data)  # 結果のJSON文字列を表示
