8281ebddcad347a36b6af4251df9c55f82d76bb6
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-15 21:18:22 +0900

    ログの配色の色相を抽出する機能の実装

--- tmp/estimate_used_color_scheme/estimate_color_scheme_method.py
+++ tmp/estimate_used_color_scheme/estimate_color_scheme_method.py
@@ -1,11 +1,33 @@
+import json
+from estimate_used_color_scheme import rgb_to_hsl
+from utils.color_utils import hex_to_rgb
 
 # ある配色で使われた配色技法を推定する関数
-def estimate_color_scheme_method(color_scheme):
-    print("called")
+
+
+def estimate_color_scheme_method(json_color_scheme):
+    # print(color_scheme)
+
+    for json_color in json_color_scheme:
+        # print(json_color)
+        color = json_color['color']
+        color_rgb = hex_to_rgb(color)
+        color_hsl = rgb_to_hsl(color_rgb)
+        hue = color_hsl[0]
+        print(f"hsl = {hue}")
+
+    print("")
 
 
 def main():
-    print("init")
+
+    with open("tmp/estimate_used_color_scheme/data/output/log_used_color_scheme.json", "r") as file:
+        json_data = json.load(file)
+
+    for color_scheme in json_data:
+        estimate_color_scheme_method(color_scheme)
+
+    # print(json_data)
 
 
 if __name__ == "__main__":
