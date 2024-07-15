3d5d8a70e80714afb126e975f2685c9ab235b40d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-15 17:52:54 +0900

    json形式のデータを保存する機能の初期実装

--- tmp/estimate_used_color_scheme/generate_json_used_color_scheme.py
+++ tmp/estimate_used_color_scheme/generate_json_used_color_scheme.py
@@ -1,9 +1,24 @@
 from estimate_used_color_scheme import estimate_used_color_scheme
+import json
+import numpy as np
 
 
 # 読込んだイラストの使用配色をjson形式で保存する関数
 def generate_json_used_color_scheme(image_path):
-    estimate_used_color_scheme(image_path)
+    used_color_schemes = estimate_used_color_scheme(image_path)
+    print(f"used_color_schemes = { used_color_schemes}")
+
+    # JSON用のリストを作成
+    json_data = []
+    for color_scheme in used_color_schemes:
+        color_dict = {
+            "color": color_scheme[0].tolist(),  # NumPy配列をリストに変換
+            "amount": color_scheme[1]
+        }
+        json_data.append(color_dict)
+
+    json_string = json.dumps(json_data, indent=4)  # JSON文字列に変換
+    print(json_string)  # 結果のJSON文字列を表示
 
 
 def main():
