3bfe8e7bd9740f18e66144399e7fd94e65af07d4
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-16 21:08:37 +0900

    ある配色で使われた色相を推定する機能を実装

--- tmp/estimate_used_color_scheme/estimate_color_scheme_method.py
+++ tmp/estimate_used_color_scheme/estimate_color_scheme_method.py
@@ -1,6 +1,7 @@
 import json
+from PIL import Image
 from estimate_used_color_scheme import rgb_to_hsl
-from utils.color_utils import hex_to_rgb, print_colored_text, hsl_to_rgb, rgb_to_hex
+from utils.color_utils import hex_to_rgb, print_colored_text, hsl_to_rgb, rgb_to_hex, merge_similar_color
 
 # ある配色で使われた配色技法を推定する関数
 
@@ -8,23 +9,43 @@ from utils.color_utils import hex_to_rgb, print_colored_text, hsl_to_rgb, rgb_to
 def estimate_color_scheme_method(json_color_scheme):
     # print(color_scheme)
     hue_array = []
+    used_color_schemes_method = []
 
     for json_color in json_color_scheme:
         # print(json_color)
-        color = json_color['color']
-        color_rgb = hex_to_rgb(color)
+        color_info = json_color['color']
+        rate = json_color['rate']
+        color_rgb = hex_to_rgb(color_info)
         color_hsl = rgb_to_hsl(color_rgb)
         hue = color_hsl[0]
         saturation = color_hsl[1]
-        if (saturation > 10):
+        lightness = color_hsl[2]
+
+        # 彩度が10(暫定値)以上の場合，有彩色としてスロットに追加
+        if ((saturation > 10) & (lightness < 95)):
             hue_array.append(hue)
+            used_color_schemes_method.append([hsl_to_rgb(hue, 50, 50), rate])
 
-    for hue in hue_array:
-        color_rgb = hsl_to_rgb(hue, 50, 50)
-        print_colored_text("■■■■■■", color_rgb)
-        print(f"hue: {hue}")
+    # 読込んだ画像の情報の出力
+    file_path = json_color_scheme[0]['illustName']
+    # img = Image.open(file_path)
+    # img.show()
+    print(file_path)
 
-    print("")
+    if (False):
+        for color_info in used_color_schemes_method:
+            print_colored_text("■■■■■■", color_info[0])
+            print(f"hsl: {rgb_to_hsl(color_info[0])}, rate: {color_info[1]}")
+
+        print("---- ↓ -----")
+
+    merged_used_color_schemes_method = merge_similar_color(used_color_schemes_method, 5)
+
+    for color_info in merged_used_color_schemes_method:
+        print_colored_text("■■■■■■", color_info[0])
+        print(f"hsl: {rgb_to_hsl(color_info[0])}, rate: {color_info[1]}")
+
+    print("\n")
 
 
 def main():
