6ca3a74fa713bb8edd24a5ca7e996642fa452c8a
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-18 01:31:04 +0900

    使用配色をコンソール上に表示させる機能の実装

--- tmp/color_recommendation/generate_recommend_color_schemes.py
+++ tmp/color_recommendation/generate_recommend_color_schemes.py
@@ -1,14 +1,22 @@
 import json
+from utils.color_utils import print_colored_text, hex_to_rgb
 
 
 # ある塗りかけの配色を基に配色を推薦する関
 def generate_recommend_color_schemes(file_path):
-    print("init")
-
     with open(file_path, 'r') as file:
-        data = json.load(file)
+        json_data = json.load(file)
 
-    print(data)
+    # print(json_data)
+
+    for used_colors_info in json_data:
+        # print(used_colors_info)
+        for color_info in used_colors_info:
+            color_rgb = hex_to_rgb(color_info['color'])
+            # print(color_rgb)
+            print_colored_text("■■■■", color_rgb)
+            print(f"color: {color_info['color']}, rate: {color_info['rate']}")
+        print("")
 
 
 def main():
