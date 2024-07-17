50ca67ee9f0f2755a292045db59b37266b53a0a2
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-18 01:06:53 +0900

    ある塗りかけの配色を基に配色を推薦する関数の初期実装

---
+++ tmp/color_recommendation/generate_recommend_color_schemes.py
@@ -0,0 +1,11 @@
+# ある塗りかけの配色を基に配色を推薦する関数
+def generate_recommend_color_schemes():
+    print("init")
+
+
+def main():
+    generate_recommend_color_schemes()
+
+
+if __name__ == "__main__":
+    main()
