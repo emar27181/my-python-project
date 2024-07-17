aca3d1efc3856576bafc0bb793d13deaa86c113c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-18 01:14:14 +0900

    ファイルを読み込む機能の実装

--- tmp/color_recommendation/generate_recommend_color_schemes.py
+++ tmp/color_recommendation/generate_recommend_color_schemes.py
@@ -1,10 +1,18 @@
-# ある塗りかけの配色を基に配色を推薦する関数
-def generate_recommend_color_schemes():
+import json
+
+
+# ある塗りかけの配色を基に配色を推薦する関
+def generate_recommend_color_schemes(file_path):
     print("init")
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    print(data)
 
 
 def main():
-    generate_recommend_color_schemes()
+    generate_recommend_color_schemes('tmp/color_recommendation/data/input/log_used_color_scheme_NCG.json')
 
 
 if __name__ == "__main__":
