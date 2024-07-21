b63c2aa6c2bf3079c1cac51e3c542fddd190688c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-21 20:52:32 +0900

    単連結法と完全連結法によるクラスタリングを引数によって切り替える機能の実装

--- tmp/clustering/clustering.py
+++ tmp/clustering/clustering.py
@@ -3,13 +3,20 @@ import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, fcluster
 
 
-def plot_clustering():
+def plot_clustering(clustring_kind):
     # 元のデータセット
     original_data = np.array([
+        [0, 4],  # A
+        [3, 1],  # B
+        [4, 1],  # C
+        [6, 3],  # D
+        [4, 4],  # E
+
         [1, 2],
         [2, 3],
         [3, 4],
         [3, 2],
+        [3, 5],
 
         [5, 6],
         [5, 5],
@@ -17,32 +24,44 @@ def plot_clustering():
         [4, 6],
         [6, 6],
 
-        [8, 8],
-        [8, 8],
-        [8, 9],
-        [8, 7],
-        [7, 8],
     ])
 
-    # 任意の10点を追加
-    # additional_data = np.random.rand(10, 2) * 10  # 10x2のランダムデータ
-    # data = np.vstack((original_data, additional_data))
     data = np.vstack((original_data))
 
     # 単連結法でクラスタリング
-    Z = linkage(data, method='single', metric='euclidean')
-    clusters = fcluster(Z, t=1.5, criterion='distance')  # クラスタ数を指定する場合、criterion='maxclust'
+    if (clustring_kind == "single"):
+        Z = linkage(data, method='single', metric='euclidean')
+    # 完全連結法でクラスタリング
+    elif (clustring_kind == "complete"):
+        Z = linkage(data, method='complete', metric='euclidean')
+
+    clusters = fcluster(Z, t=1.5, criterion='distance')  # 距離を基準にクラスタリング
+    # clusters = fcluster(Z, t=3, criterion='maxclust')  # クラスタ数を基準にクラスタリング
 
     # クラスタリング結果のプロットと保存
     plt.figure(figsize=(10, 10))
-    plt.scatter(data[:, 0], data[:, 1], c=clusters, cmap='viridis')
-    plt.title("Single Linkage Clustering on XY Plane")
-    plt.xlim(0, 10)
-    plt.ylim(0, 10)
+    markers = ['o', 's', '^', 'D', 'v', 'p', 'h', 'x', '+', '*']
+    colors = ['red', 'green', 'blue', 'cyan', 'magenta', 'black', 'orange', 'purple', 'pink']
+
+    for cluster in np.unique(clusters):
+        cluster_data = data[clusters == cluster]
+        plt.scatter(cluster_data[:, 0], cluster_data[:, 1],
+                    # c=[cluster] * len(cluster_data), cmap='viridis',
+                    color=colors[cluster % len(colors)],
+                    marker=markers[cluster % len(markers)],
+                    label=f'Cluster {cluster}',
+
+                    )
+
+    # plt.scatter(data[:, 0], data[:, 1], c=clusters, cmap='viridis')
+    plt.title(f"{clustring_kind} Linkage Clustering on XY Plane")
+    plt.grid(True)
+    plt.xlim(-1, 8)
+    plt.ylim(-1, 8)
     plt.xlabel("X")
     plt.ylabel("Y")
 
-    file_name = "/mnt/c/WSL-directory/my-NLP-project/tmp/clustering/data/output/single_linkage_dendrogram.png"
+    file_name = (f"/mnt/c/WSL-directory/my-NLP-project/tmp/clustering/data/output/{clustring_kind}_linkage_xy.png")
     plt.savefig(f"{file_name}")
 
     print(f"{file_name} が保存されました．")
@@ -51,7 +70,8 @@ def plot_clustering():
 
 
 def main():
-    plot_clustering()
+    plot_clustering("single")
+    plot_clustering("complete")
 
 
 if __name__ == "__main__":
