04e5aa32d4d2a8ad5137168d0c2eef12ed5660c2
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-21 19:46:33 +0900

    クラスタリング結果をプロットする関数とmain関数の実装

--- tmp/clustering/clustering.py
+++ tmp/clustering/clustering.py
@@ -2,47 +2,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, fcluster
 
-# 元のデータセット
-original_data = np.array([
-    [1, 2],
-    [2, 3],
-    [3, 4],
-    [3, 2],
 
-    [5, 6],
-    [5, 5],
-    [6, 5],
-    [4, 6],
-    [6, 6],
+def plot_clustering():
+    # 元のデータセット
+    original_data = np.array([
+        [1, 2],
+        [2, 3],
+        [3, 4],
+        [3, 2],
 
-    [8, 8],
-    [8, 8],
-    [8, 9],
-    [8, 7],
-    [7, 8],
-])
+        [5, 6],
+        [5, 5],
+        [6, 5],
+        [4, 6],
+        [6, 6],
 
-# 任意の10点を追加
-# additional_data = np.random.rand(10, 2) * 10  # 10x2のランダムデータ
-# data = np.vstack((original_data, additional_data))
-data = np.vstack((original_data))
+        [8, 8],
+        [8, 8],
+        [8, 9],
+        [8, 7],
+        [7, 8],
+    ])
 
-# 単連結法でクラスタリング
-Z = linkage(data, method='single', metric='euclidean')
-clusters = fcluster(Z, t=1.5, criterion='distance')  # クラスタ数を指定する場合、criterion='maxclust'
+    # 任意の10点を追加
+    # additional_data = np.random.rand(10, 2) * 10  # 10x2のランダムデータ
+    # data = np.vstack((original_data, additional_data))
+    data = np.vstack((original_data))
 
-# クラスタリング結果のプロットと保存
-plt.figure(figsize=(10, 10))
-plt.scatter(data[:, 0], data[:, 1], c=clusters, cmap='viridis')
-plt.title("Single Linkage Clustering on XY Plane")
-plt.xlim(0, 10)
-plt.ylim(0, 10)
-plt.xlabel("X")
-plt.ylabel("Y")
+    # 単連結法でクラスタリング
+    Z = linkage(data, method='single', metric='euclidean')
+    clusters = fcluster(Z, t=1.5, criterion='distance')  # クラスタ数を指定する場合、criterion='maxclust'
 
-file_name = "/mnt/c/WSL-directory/my-NLP-project/tmp/clustering/data/output/single_linkage_dendrogram.png"
-plt.savefig(f"{file_name}")
+    # クラスタリング結果のプロットと保存
+    plt.figure(figsize=(10, 10))
+    plt.scatter(data[:, 0], data[:, 1], c=clusters, cmap='viridis')
+    plt.title("Single Linkage Clustering on XY Plane")
+    plt.xlim(0, 10)
+    plt.ylim(0, 10)
+    plt.xlabel("X")
+    plt.ylabel("Y")
 
-print(f"{file_name} が保存されました．")
+    file_name = "/mnt/c/WSL-directory/my-NLP-project/tmp/clustering/data/output/single_linkage_dendrogram.png"
+    plt.savefig(f"{file_name}")
 
-plt.close()
+    print(f"{file_name} が保存されました．")
+
+    plt.close()
+
+
+def main():
+    plot_clustering()
+
+
+if __name__ == "__main__":
+    main()
