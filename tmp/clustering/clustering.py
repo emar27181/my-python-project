1c17b8c3613d40796017d1475c8afc926c4afa53
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-23 02:38:32 +0900

    完全連結法によるクラスタリングの実装

--- tmp/clustering/clustering.py
+++ tmp/clustering/clustering.py
@@ -6,6 +6,39 @@ from scipy.spatial.distance import pdist, squareform
 from sklearn.cluster import KMeans
 
 
+# 完全連結法によるクラスタリング
+def complete_linkage(X):
+    n = len(X)
+    Z = np.zeros((n - 1, 4))  # 結果を格納する配列
+    D = squareform(pdist(X))  # 距離行列
+    np.fill_diagonal(D, np.inf)  # 対角要素は無限大にする
+
+    clusters = {i: [i] for i in range(n)}  # 各データポイントをクラスタに格納
+    cluster_indices = {i: i for i in range(n)}  # 各データポイントのインデックス
+
+    for k in range(n - 1):
+        i, j = np.unravel_index(np.argmin(D), D.shape)  # 最小距離を持つクラスタの探索
+        min_dist = D[i, j]  # 最小距離
+
+        # クラスタの結合
+        new_cluster = clusters[i] + clusters[j]
+        Z[k] = [cluster_indices[i], cluster_indices[j], min_dist, len(new_cluster)]
+
+        # 距離行列の更新
+        for m in range(n):
+            if m != i and m != j:
+                D[i, m] = D[m, i] = max(D[i, m], D[j, m])
+
+        D[j, :] = D[:, j] = np.inf  # j行とj列を無限大に設定
+
+        # クラスタの管理
+        clusters[i] = new_cluster
+        clusters[j] = []  # jクラスタを空にすることで再利用を防ぐ
+        cluster_indices[i] = k + n  # 新しいクラスタのインデックスを設定
+
+    return Z
+
+
 # 単連結法によるクラスタリング
 def single_linkage(X):
     n = len(X)
@@ -76,7 +109,8 @@ def plot_clustering(clustring_method):
             Z = single_linkage(data)
         # 完全連結法でクラスタリング
         elif (clustring_method == "complete"):
-            Z = linkage(data, method='complete', metric='euclidean')
+            # Z = linkage(data, method='complete', metric='euclidean')
+            Z = complete_linkage(data)
 
         clusters = fcluster(Z, t=1.5, criterion='distance')  # 距離を基準にクラスタリング
         # clusters = fcluster(Z, t=3, criterion='maxclust')  # クラスタ数を基準にクラスタリング
