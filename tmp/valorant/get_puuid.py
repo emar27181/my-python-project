cb233c3036e220e1f7f48a383ac10b231b2755e8
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-03 17:43:56 +0900

    マッチリストを取得する関数の仮実装

--- tmp/valorant/get_puuid.py
+++ tmp/valorant/get_puuid.py
@@ -4,7 +4,7 @@ from config.constants_dev import API_KEY
 
 # プレイヤーのpuuidを取得する関数
 def get_player_puuid(api_key, game_name, tag_line):
-    url = f'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}'
+    url = f'https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}'
     print(f"url: {url}")
 
     headers = {
@@ -21,9 +21,32 @@ def get_player_puuid(api_key, game_name, tag_line):
         return None
 
 
+# マッチリストを取得する関数
+# マッチまでアクセス可能なAPIは個人利用で取得できない？(2024/08/03)
+def get_recent_matches(api_key, puuid, count=10):
+    url = f'https://ap.api.riotgames.com/val/match/v1/matchlists/by-puuid/{puuid}'
+    print(f"url: {url}")
+    params = {
+        'start': 0,
+        'count': count
+    }
+    headers = {
+        'X-Riot-Token': api_key
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to get match list. Status code: {response.status_code}, Response: {response.text}")
+        return None
+
+
 def main():
     puuid = get_player_puuid(API_KEY, "nodoame", "27181")
     print(f"puuid: {puuid}")
+    get_recent_matches(API_KEY, puuid)
 
 
 if __name__ == "__main__":
