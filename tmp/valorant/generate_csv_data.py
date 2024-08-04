eebaa76c6bdddfa4e20534b80849149722bcfb38
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-04 20:31:29 +0900

    辞書型でデータを保存するように変更

--- tmp/valorant/generate_csv_data.py
+++ tmp/valorant/generate_csv_data.py
@@ -1,12 +1,25 @@
 
 def read_file_to_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        data = file.read()
-    return data
+        lines = file.read().splitlines()
+
+    data_list = []
+    for i in range(0, len(lines), 14):
+        person_dict = {
+            'Name': lines[i + 1],
+            'KD': lines[i + 2],
+            'WinRate': lines[i + 8],
+            'PickRate': lines[i + 10],
+        }
+        data_list.append(person_dict)
+
+    return data_list
 
 
 def main():
-    data = read_file_to_data('tmp/valorant/data/input/epi9_act1_allmap_bronze3.txt')
+    # data = read_file_to_data('tmp/valorant/data/input/epi9_act1_allmap_bronze3.txt')
+    data = read_file_to_data('tmp/valorant/data/input/epi9_act1_allmap_platinum3.txt')
+    # data = read_file_to_data('tmp/valorant/data/input/epi9_act1_allmap_immortal3.txt')
     print(data)
 
 
