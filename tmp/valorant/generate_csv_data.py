0b02cb121c9875f96f22e2db2c26ae331c2e3232
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-04 20:17:10 +0900

    試合データを読み込む関数の初期実装

--- tmp/valorant/generate_csv_data.py
+++ tmp/valorant/generate_csv_data.py
@@ -1,6 +1,13 @@
 
+def read_file_to_data(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = file.read()
+    return data
+
+
 def main():
-    print("hello world!")
+    data = read_file_to_data('tmp/valorant/data/input/epi9_act1_allmap_bronze3.txt')
+    print(data)
 
 
 if __name__ == "__main__":
