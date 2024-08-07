bfef68ee9aecdaccb2cdbf6701685f76654edac8
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-07 20:51:40 +0900

    読込んだjson形式の色データを辞書型で保存する関数の実装

--- tmp/estimate_used_color_scheme/generate_csv_data.py
+++ tmp/estimate_used_color_scheme/generate_csv_data.py
@@ -1,11 +1,25 @@
 import json
+import csv
 
 
 def generate_csv_used_color_scheme(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         json_data = json.load(file)
 
-    print(f"json_data: {json_data}")
+    used_color_list = []
+
+    for color_scheme in json_data:
+        for color_data in color_scheme:
+            # print(f"color_data: {color_data}")
+            add_dict = {
+                'Color': color_data['color'],
+                'Rate': color_data['rate'],
+                'IllustName': color_scheme[0]['illustName'],
+            }
+            # print(f"add_dict: {add_dict}")
+            used_color_list.append(add_dict)
+
+    print(f"used_color_list: {used_color_list}")
 
 
 def main():
