23c2b487a8f9d12b9d93671bb844647dd4806447
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-07 20:33:41 +0900

    使用した配色のjsonデータを読み込む昨日の実装

--- tmp/estimate_used_color_scheme/generate_csv_data.py
+++ tmp/estimate_used_color_scheme/generate_csv_data.py
@@ -1,9 +1,15 @@
-def generate_csv_used_color_scheme():
-    print("hello world")
+import json
+
+
+def generate_csv_used_color_scheme(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    print(f"json_data: {json_data}")
 
 
 def main():
-    generate_csv_used_color_scheme()
+    generate_csv_used_color_scheme('tmp/estimate_used_color_scheme/data/output/log_used_color_scheme_gaako_instagram.json')
 
 
 if __name__ == "__main__":
