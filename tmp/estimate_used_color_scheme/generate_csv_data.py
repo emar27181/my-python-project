4da160f86b77e9b5617d73f3f36692eb25ca3c0d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-07 21:11:52 +0900

    HSL値を辞書型で保存する機能の実装

--- tmp/estimate_used_color_scheme/generate_csv_data.py
+++ tmp/estimate_used_color_scheme/generate_csv_data.py
@@ -1,5 +1,6 @@
 import json
 import csv
+from utils.color_utils import hex_to_rgb, rgb_to_hsl
 
 
 def generate_csv_used_color_scheme(file_path):
@@ -10,9 +11,20 @@ def generate_csv_used_color_scheme(file_path):
 
     for color_scheme in json_data:
         for color_data in color_scheme:
+
+            color_hex = color_data['color']
+            color_rgb = hex_to_rgb(color_hex)
+            print(f"color_rgb = {color_rgb}")
+            color_hsl = rgb_to_hsl(color_rgb)
+            print(f"color_hsl = { color_hsl }")
+            print("")
             # print(f"color_data: {color_data}")
+
             add_dict = {
                 'Color': color_data['color'],
+                'Hue': color_hsl[0],
+                'Saturation': color_hsl[1],
+                'Lightness': color_hsl[2],
                 'Rate': color_data['rate'],
                 'IllustName': color_scheme[0]['illustName'],
             }
