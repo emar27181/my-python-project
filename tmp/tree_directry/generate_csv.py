0f0aa98c42070d5a3c36d5760adef436026ec99f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-09 14:57:07 +0900

    ディレクトリ構造を保存するテキストデータを読み込む初期実装

--- tmp/tree_directry/generate_csv.py
+++ tmp/tree_directry/generate_csv.py
@@ -1,5 +1,17 @@
+
+
+def read_tree_file_to_data(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.read().splitlines()
+
+    lines = [line.replace('-', '') for line in lines]
+    lines = [line.replace(',', '') for line in lines]
+
+    print(lines)
+
+
 def generate_csv():
-    print("test")
+    read_tree_file_to_data('tmp/tree_directry/data/input/tree_emar27181_github_io.txt')
 
 
 def main():
