53de870fc31a00f351444cdc2c99cf11e57b7181
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-09 11:10:59 +0900

    アビリティに関するテキストデータを辞書型リストに変換する関数の実装

--- tmp/valorant/generate_csv_data.py
+++ tmp/valorant/generate_csv_data.py
@@ -50,6 +50,24 @@ def read_rifle_file_to_data(file_path, load_rank):
     return data_list
 
 
+def read_agent_avility_file_to_data(file_path, load_rank):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.read().splitlines()
+
+    data_list = []
+    for i in range(0, len(lines), 14):
+        person_dict = {
+            'Name': lines[i + 1],
+            'KD': lines[i + 2],
+            'WinRate': _percent_normalize(lines[i + 8]),
+            'PickRate': _percent_normalize(lines[i + 10]),
+            'Rank': load_rank,
+        }
+        data_list.append(person_dict)
+
+    return data_list
+
+
 # 引数で受け取ったライフルに関するデータのCSVファイルを生成する関数
 def generate_rifle_csv_data(load_rank_list):
     data = []
